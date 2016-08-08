95ed5abb2e40e08ef3aacd4275074734b33052e0
lq326065285 <chenhongyu@qianxiao.local>
2016-08-08 10:12:32 +0800

    Initial Commit

---
+++ CAShapeLayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CAShapeLayerDemo
+//
+//  Created by ~~浅笑 on 16/8/8.
+//  Copyright © 2016年 广东康爱多连锁药店有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
